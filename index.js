7b320b0409173630a8f7d173bf8a7fb8365af9ae
Hubert Berja <bert.stark3231@gmail.com>
2022-04-24 18:45:19 +0800

    status 200

--- index.js
+++ index.js
@@ -15,20 +15,50 @@ $(function () {
       }
     });
 
-    lineArr.forEach((url) => {
-      getStatus(url);
-    });
+    // function checkStatus(url) {
+    //   const response = fetch(url, {
+    //     mode: "no-cors",
+    //     cache: "no-cache",
+    //     credentials: "same-origin",
+    //     redirect: "follow",
+    //     headers: {
+    //       "Access-Control-Allow-Origin": "*",
+    //       "Content-Type": "application/json",
+    //     },
+    //   }).then((response) => response.json());
+    //   return response;
+    // }
 
-    async function getStatus(url) {
-      const result = await fetch(url, {
-        method: "GET",
-        mode: "no-cors",
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-        },
-      }).then((response) => console.log(response));
+    // lineArr.forEach((url) => {
+    //   checkStatus(url).then((data) => {
+    //     console.log(data);
+    //   });
+    // });
 
-      return result;
+    async function makeRequest() {
+      try {
+        const response = await fetch("http://libopac.nodai.ac.jp/", {
+          method:"POST",
+          mode: "no-cors",
+          redirect: "follow",
+        });
+
+        console.log("status code: ", response.status); // 👉️ 200
+
+        if (!response.ok) {
+          console.log(response);
+          throw new Error(`Error! status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        return result;
+      } catch (err) {
+        console.log(err);
+      }
     }
+
+    makeRequest();
   });
 });
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#uploading_json_data
